d1cb699c6a027d791d76468987d8f963ffdd2185
ghh-jb <138157456+ghh-jb@users.noreply.github.com>
2026-04-27 12:32:24 +0300

    There it goes I suppose

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 cc ./md5.c -o md5sum
-mv ./md5sum /usr/local/bin/
-chown root:root /usr/local/bin/md5sum
-chmod 755 /usr/local/bin/md5sum
+cp /usr/bin/md5sum /usr/bin/md5
+mv ./md5sum /usr/bin/
+chown root:root /usr/bin/md5sum
+chmod 755 /usr/bin/md5sum
